a746cb4bc092b3fe5f16ce9d9868015029d74591
정해준 <jujng@naver.com>
2024-10-29 13:26:25 +0900

    feat : jwt 진행

--- backend/src/main/java/com/backend/controller/UserController.java
+++ backend/src/main/java/com/backend/controller/UserController.java
@@ -5,9 +5,8 @@ import com.backend.dto.SignDTO;
 import com.backend.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.RequestEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/users")
@@ -15,13 +14,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private final UserService userService;
 
-    @RequestMapping("/signup")
-    public RequestEntity<String> signup(@RequestBody SignDTO signDTO){
+    @PostMapping("/signup")
+    public ResponseEntity<String> signup(@RequestBody SignDTO signDTO){
+        userService.signup(signDTO);
 
+        return ResponseEntity.ok().body("Sign up successful");
     }
 
-    @RequestMapping("/login")
-    public RequestEntity<String> login(@RequestBody LoginDTO loginDTO){
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginDTO loginDTO){
+        userService.login(loginDTO);
+        //로그인 되었을 때 토큰 발급
+        return ResponseEntity.ok().body("Sign up successful");
+    }
 
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(@RequestParam String id) {
+        userService.logout(id);
+        return ResponseEntity.ok("로그아웃이 완료되었습니다.");
     }
 }
