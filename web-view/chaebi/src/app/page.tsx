7313912d89fa88bfea9d367eb8d3c423d1df3256
Yoonha <yoonha0124@naver.com>
2024-11-01 14:02:45 +0900

    Feat: primary 폰트 색 지정

--- web-view/chaebi/src/app/page.tsx
+++ web-view/chaebi/src/app/page.tsx
@@ -4,10 +4,10 @@ export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
       <Chaebi />
-      <div className="mt-5 text-xl text-gray-700 mb-1">
+      <div className="mt-5 text-xl text-primary mb-1">
         남은 이들을 위한 채비
       </div>
-      <div className="text-xl text-gray-700 mb-8">채우고, 비우기</div>
+      <div className="text-xl text-primary mb-8">채우고, 비우기</div>
     </div>
   )
 }
--- web-view/chaebi/tailwind.config.ts
+++ web-view/chaebi/tailwind.config.ts
@@ -4,12 +4,22 @@ const config: Config = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
+      fontFamily: {
+        LeeSeoyun: ['var(--font-LeeSeoyun)', 'sans-serif'],
+      },
       colors: {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
-      },
-      fontFamily: {
-        LeeSeoyun: ['var(--font-LeeSeoyun)', 'sans-serif'],
+        primary: '#444444',
+        // _red: '#EA4141',
+        // '_blue-100': '#8CD2F5',
+        // '_blue-200': '#4BAFF5',
+        // '_blue-300': '#2878F5',
+        // '_blue-400': '#00236E',
+        // '_grey-100': '#F4F4F4',
+        // '_grey-200': '#D2D2D2',
+        // '_grey-300': '#929292',
+        // '_grey-400': '#626262',
       },
     },
   },
