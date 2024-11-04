c78729d4605b1f83d28392e6a439e4d4072928f3
권주안 <wndks6450@gmail.com>
2024-11-04 11:17:09 +0900

    style : phone auth design

--- chaebi/src/screens/signin/index.jsx
+++ chaebi/src/screens/signin/index.jsx
@@ -8,37 +8,52 @@ export default function SignInScreen() {
   const [showAuth, setShowAuth] = useState(false);
 
   return (
-    <View style={{backgroundColor: 'white'}}>
+    <View style={{flex: 1, backgroundColor: 'white'}}>
       <HeaderComp pageName={'로그인'} />
       <View style={{marginTop: 32, gap: 36}}>
         <View style={{paddingHorizontal: 24, gap: 20}}>
           <Text style={{fontSize: 24, fontFamily: '이서윤체'}}>
-            휴대폰 번호로 로그인해주세요.
+            {!showAuth
+              ? '휴대폰 번호로 로그인해주세요.'
+              : '인증번호를 입력해주세요.'}
           </Text>
           <InputFieldComp
             placeholder={'휴대폰 번호'}
             keyboardType={'phone-pad'}
+            disabled={showAuth}
+            isPhoneNum={true}
           />
-          <RoundButtonComp
-            content={'인증번호 발송'}
-            onPress={() => setShowAuth(true)}
-          />
+          {!showAuth ? (
+            <RoundButtonComp
+              content={'인증번호 발송'}
+              onPress={() => setShowAuth(true)}
+              disabled={showAuth}
+            />
+          ) : (
+            <></>
+          )}
         </View>
         {showAuth ? (
           <View style={{paddingHorizontal: 24, gap: 20}}>
-            <Text style={{fontSize: 24, fontFamily: '이서윤체'}}>
+            {/* <Text style={{fontSize: 24, fontFamily: '이서윤체'}}>
               인증번호를 입력해주세요.
-            </Text>
+            </Text> */}
             <InputFieldComp
               placeholder={'인증번호'}
               keyboardType={'phone-pad'}
             />
-
             <RoundButtonComp content={'인증하기'} />
+            <Text
+              style={{
+                fontSize: 24,
+                fontFamily: '이서윤체',
+                textAlign: 'center',
+                marginTop: 28,
+              }}>
+              인증번호 재전송
+            </Text>
           </View>
-        ) : (
-          <></>
-        )}
+        ) : null}
       </View>
     </View>
   );
