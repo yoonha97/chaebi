749518d08c50497c5bd0f8a86202b35839b891b7
권주안 <wndks6450@gmail.com>
2024-11-04 12:48:43 +0900

    feat : install asyncstorage

--- chaebi/App.jsx
+++ chaebi/App.jsx
@@ -1,25 +1,46 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import SplashScreen from './src/screens/splash';
 import SignInScreen from './src/screens/signin';
 
 const stack = createNativeStackNavigator();
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isFirstLaunch, setIsFirstLaunch] = useState(null);
+
+  useEffect(() => {
+    const checkFirstLaunch = async () => {
+      try {
+        const hasLaunched = await AsyncStorage.getItem('hasLaunched');
+        if (hasLaunched === null) {
+          // 앱을 처음 실행한 경우
+          setIsFirstLaunch(true);
+          await AsyncStorage.setItem('hasLaunched', 'true');
+        } else {
+          setIsFirstLaunch(false);
+        }
+      } catch (error) {
+        console.log("Error checking app's launch status:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    checkFirstLaunch();
+  }, []);
+
   return (
     <NavigationContainer>
-      <stack.Navigator>
+      <stack.Navigator screenOptions={{headerShown: false}}>
         <stack.Screen
           name={'Splash'}
           component={SplashScreen}
-          options={{headerShown: false}}
         />
         <stack.Screen
           name={'SignIn'}
           component={SignInScreen}
-          options={{headerShown: false}}
         />
       </stack.Navigator>
     </NavigationContainer>
--- chaebi/src/screens/splash/index.jsx
+++ chaebi/src/screens/splash/index.jsx
@@ -5,7 +5,7 @@ import Logo from '../../assets/logo/logo.svg';
 export default function SplashScreen({navigation}) {
   setTimeout(() => {
     navigation.replace('SignIn');
-  }, 500);
+  }, 1000);
 
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
