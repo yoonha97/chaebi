49f205453fcb4271bd72bd6aa1b9ec2a70399294
권주안 <wndks6450@gmail.com>
2024-11-05 11:17:09 +0900

    style : absence page 컴포넌트 자인

--- chaebi/src/components/RoundButtonComp.tsx
+++ chaebi/src/components/RoundButtonComp.tsx
@@ -7,6 +7,7 @@ interface RoundButtonCompProps {
   disabled?: boolean;
   backgroundColor?: string;
   fontColor?: string;
+  border?: string;
 }
 
 export default function RoundButtonComp({
@@ -15,13 +16,14 @@ export default function RoundButtonComp({
   disabled = false,
   backgroundColor = 'bg-[#444444]',
   fontColor = 'text-[#ffffff]',
+  border,
 }: RoundButtonCompProps) {
   return (
     <TouchableOpacity onPress={onPress} disabled={disabled}>
       <View
         className={`rounded-lg py-5 ${
           disabled ? 'bg-[#bbbbbb]' : backgroundColor
-        }`}>
+        } ${border ? border : ''}`}>
         <Text
           className={`font-['이서윤체'] text-2xl text-center ${
             disabled ? 'text-[#888888]' : fontColor
--- chaebi/src/screens/absence/index.tsx
+++ chaebi/src/screens/absence/index.tsx
@@ -11,22 +11,30 @@ type AbsenceScreenProps = {
 
 export default function AbsenceScreen({navigation}: AbsenceScreenProps) {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Logo width={150} height={120} />
-      <Text className="text-lg font-['이서윤체'] text-center mt-3">
-        남은 이들을 위한 채비
-      </Text>
-      <Text className="text-lg font-['이서윤체'] text-center mt-2">
-        채우고, 비우기
-      </Text>
-      <View className="flex-1 justify-end w-ufll">
+    <View className="flex-1 p-5">
+      <View className="flex-1 justify-center items-center">
+        <Logo width={150} height={120} />
+        <Text className="text-lg font-['이서윤체'] text-center mt-3">
+          남은 이들을 위한 채비
+        </Text>
+        <Text className="text-lg font-['이서윤체'] text-center mt-2">
+          채우고, 비우기
+        </Text>
+      </View>
+      <View className="justify-end w-full gap-5">
         <RoundButtonComp
           content={'박수진님이신가요?'}
           onPress={() => {
             navigation.navigate('SignIn');
           }}
         />
-        <RoundButtonComp content={'유족이신가요?'} onPress={() => {}} />
+        <RoundButtonComp
+          content={'유족이신가요?'}
+          onPress={() => {}}
+          backgroundColor={'bg-[#D9D9D9]'}
+          fontColor={'text-[#444444]'}
+          border={'border-2 border-[#444444]'}
+        />
       </View>
     </View>
   );
