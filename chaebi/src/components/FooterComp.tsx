69f4a5b7ab4828eb82e09178321c6581d1e625fc
권주안 <wndks6450@gmail.com>
2024-11-06 10:16:05 +0900

    style : footer design

--- chaebi/src/components/FooterComp.tsx
+++ chaebi/src/components/FooterComp.tsx
@@ -1,20 +1,28 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import Home from '../assets/icon/home.svg';
+import Box from '../assets/icon/box.svg';
+import Layout from '../assets/icon/layout.svg';
+import Person from '../assets/icon/person.svg';
 
 export default function FooterComp() {
   return (
-    <View className="flex-row">
-      <View className="flex-1">
-        <Text className="text-center">홈</Text>
+    <View className="flex-row justify-between">
+      <View className="items-center w-20">
+        <Home width={32} height={32} />
+        <Text className="text-xs text-center text-[#D2D2D2]">홈</Text>
       </View>
-      <View className="flex-1">
-        <Text className="text-center">남기기</Text>
+      <View className="items-center w-20">
+        <Box width={32} height={32} />
+        <Text className="text-xs text-center text-[#D2D2D2]">남기기</Text>
       </View>
-      <View className="flex-1">
-        <Text className="text-center">채우기</Text>
+      <View className="items-center w-20">
+        <Layout width={32} height={32} />
+        <Text className="text-xs text-center text-[#D2D2D2]">채우기</Text>
       </View>
-      <View className="flex-1">
-        <Text className="text-center">마이페이지</Text>
+      <View className="items-center w-20">
+        <Person width={32} height={32} />
+        <Text className="text-xs text-center text-[#D2D2D2]">마이페이지</Text>
       </View>
     </View>
   );
