22872fb22808d8a26d42e644ee4d61ed5c72fcf7
정해준 <jujng@naver.com>
2024-11-08 17:28:02 +0900

    feat : firebase service add

--- backend/src/main/java/com/backend/service/firebase/FirebaseServiceImpl.java
+++ backend/src/main/java/com/backend/service/firebase/FirebaseServiceImpl.java
@@ -1,18 +1,66 @@
-//package com.backend.service.firebase;
-//
-//import com.backend.domain.User;
-//import jakarta.transaction.Transactional;
-//
-//public class FirebaseServiceImpl implements FirebaseService {
-//
-//    @Override
-//    @Transactional
-//    public String getToken(Long userId, String token) throws BaseException {
-//        // 해당 아이디 가진 유저가 존재하는지 검사
-//        User userById = userRepository.findByIdAndStatus(userId, Status.A)
-//                .orElseThrow(() -> new BaseException(BaseResponseStatus.BASE_INVALID_USER));
-//
-//        userById.setFCMToken(token);
-//        return "토큰이 성공적으로 저장되었습니다";
-//    }
-//}
+package com.backend.service.firebase;
+
+import com.backend.dto.FcmMessage;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.common.net.HttpHeaders;
+import com.google.gson.JsonParseException;
+import lombok.RequiredArgsConstructor;
+import okhttp3.*;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class FirebaseServiceImpl {
+
+    private final String API_URL = "https://fcm.googleapis.com/v1/projects/my-firebase-project-a309/messages:send";
+    private final ObjectMapper objectMapper;
+
+    public void sendMessageTo(String targetToken, String title, String body) throws IOException {
+        String message = makeMessage(targetToken, title, body);
+
+        OkHttpClient client = new OkHttpClient();
+        RequestBody requestBody = RequestBody.create(message,
+                MediaType.get("application/json; charset=utf-8"));
+        Request request = new Request.Builder()
+                .url(API_URL)
+                .post(requestBody)
+                .addHeader(HttpHeaders.AUTHORIZATION, "Bearer " + getAccessToken())
+                .addHeader(HttpHeaders.CONTENT_TYPE, "application/json; UTF-8")
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        System.out.println(response.body().string());
+    }
+
+    private String makeMessage(String targetToken, String title, String body) throws JsonParseException, JsonProcessingException {
+        FcmMessage fcmMessage = FcmMessage.builder()
+                .message(FcmMessage.Message.builder()
+                        .token(targetToken)
+                        .notification(FcmMessage.Notification.builder()
+                                .title(title)
+                                .body(body)
+                                .image(null)
+                                .build()
+                        ).build()).validateOnly(false).build();
+
+        return objectMapper.writeValueAsString(fcmMessage);
+    }
+
+    private String getAccessToken() throws IOException {
+        String firebaseConfigPath = "firebase/firebase_service_key.json";
+
+        GoogleCredentials googleCredentials = GoogleCredentials
+                .fromStream(new ClassPathResource(firebaseConfigPath).getInputStream())
+                .createScoped(List.of("https://www.googleapis.com/auth/cloud-platform"));
+
+        googleCredentials.refreshIfExpired();
+        return googleCredentials.getAccessToken().getTokenValue();
+    }
+}
