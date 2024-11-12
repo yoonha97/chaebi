bf6f3f05b43235df4092724e06cb1b12b10b730d
정해준 <jujng@naver.com>
2024-11-12 10:30:34 +0900

    feat : 열람자 SQL 수정

--- backend/src/main/java/com/backend/controller/FcmController.java
+++ backend/src/main/java/com/backend/controller/FcmController.java
@@ -1,7 +1,12 @@
 package com.backend.controller;
 
+import com.backend.domain.User;
 import com.backend.dto.RequestDTO;
+import com.backend.repository.UserRepository;
 import com.backend.service.firebase.FirebaseServiceImpl;
+import com.backend.service.user.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,16 +20,27 @@ import java.io.IOException;
 public class FcmController {
 
     private final FirebaseServiceImpl firebaseService;
+    private final UserService userService;
+    private final UserRepository userRepository;
 
+    @Operation(summary = "푸쉬 알람 테스트")
     @PostMapping("/api/fcm")
     public ResponseEntity pushMessage(@RequestBody RequestDTO requestDTO) throws IOException {
-        System.out.println(requestDTO.getTargetToken() + " "
-                +requestDTO.getTitle() + " " + requestDTO.getBody());
-
+        System.out.println(requestDTO.getTargetToken());
         firebaseService.sendMessageTo(
                 requestDTO.getTargetToken(),
-                requestDTO.getTitle(),
-                requestDTO.getBody());
+                "채비",
+                "입장하세요");
+        return ResponseEntity.ok().build();
+    }
+
+
+    @Operation(summary = "유저의 fcm 토큰을 저장")
+    @PostMapping("/api/token")
+    public ResponseEntity pushToken(@RequestBody RequestDTO requestDTO, HttpServletRequest request) throws IOException {
+        User user = userService.getUserByToken(request).get(); //  유저 정보 받아옴
+        user.setFcmToken(requestDTO.getTargetToken());
+        userRepository.save(user);
         return ResponseEntity.ok().build();
     }
 }
