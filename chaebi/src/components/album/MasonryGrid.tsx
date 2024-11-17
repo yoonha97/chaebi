f8e5f5b26f6865cc03382074d0835bac0b304602
soojin <onepst@hanyang.ac.kr>
2024-11-17 17:16:14 +0900

    feat: 사진상세보기 캐러셀

--- chaebi/src/components/album/MasonryGrid.tsx
+++ chaebi/src/components/album/MasonryGrid.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useState} from 'react';
+import {Pressable, View} from 'react-native';
 import MasonryGridItem from './MasonryGridItem';
 import {Media} from '../../types/album';
+import {useModal} from '../../hooks/useModal';
+import MediaCarouselModal from '../modal/MediaCarouselModal';
 
 interface MasonryGridProps {
   mediaList: (Media & {height: number})[];
@@ -13,15 +15,35 @@ function MasonryGrid({mediaList}: MasonryGridProps) {
     columns[index % 2].push(item);
   });
 
+  const {isVisible, openModal, closeModal} = useModal();
+  const [initialIndex, setInitialIndex] = useState(0);
+
+  const handlePress = (itemIndex: number) => {
+    setInitialIndex(itemIndex);
+    openModal();
+  };
+
   return (
     <View className="flex-row justify-between gap-2">
       {columns.map((column, colIndex) => (
         <View key={colIndex} className="flex-1">
           {column.map(item => (
-            <MasonryGridItem key={item.id} media={item} />
+            <Pressable
+              onPress={() =>
+                handlePress(mediaList.findIndex(media => media.id === item.id))
+              }
+              key={item.id}>
+              <MasonryGridItem media={item} />
+            </Pressable>
           ))}
         </View>
       ))}
+      <MediaCarouselModal
+        visible={isVisible}
+        onClose={closeModal}
+        mediaList={mediaList}
+        initialIndex={initialIndex}
+      />
     </View>
   );
 }
